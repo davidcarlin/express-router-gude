58ca5c8273d306c6a7b511221d1995a7ba74b0b2
davidcarlin <david.carlin@barclays.com>
2023-02-27 16:14:52 +0000

    post put delete handlers for users

--- routes/users.js
+++ routes/users.js
@@ -36,4 +36,45 @@ router.get("/:id", (req, res) => {
   }
 });
 
+// Create a new user
+router.post("/", (req, res) => {
+  const { name, age } = req.body;
+
+  if (!name || !age) {
+    res.status(400).send("Please provide a name and age for the user");
+  } else {
+    const newUser = { name, age };
+    users.push(newUser);
+    res.status(201).json(newUser);
+  }
+});
+
+// Update an existing user
+router.put("/:id", (req, res) => {
+  const id = req.params.id;
+  const { name, age } = req.body;
+
+  if (id < 1 || id > users.length) {
+    res.status(404).send("User not found");
+  } else if (!name || !age) {
+    res.status(400).send("Please provide a name and age for the user");
+  } else {
+    const updatedUser = { name, age };
+    users[id - 1] = updatedUser;
+    res.json(updatedUser);
+  }
+});
+
+// Remove an existing user
+router.delete("/:id", (req, res) => {
+  const id = req.params.id;
+
+  if (id < 1 || id > users.length) {
+    res.status(404).send("User not found");
+  } else {
+    users.splice(id - 1, 1);
+    res.sendStatus(204);
+  }
+});
+
 module.exports = router;
