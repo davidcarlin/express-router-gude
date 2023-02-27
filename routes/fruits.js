437ee199217b3c755bffface5863bcdb24757858
davidcarlin <david.carlin@barclays.com>
2023-02-27 15:54:51 +0000

    fruits routers defined

--- routes/fruits.js
+++ routes/fruits.js
@@ -21,12 +21,19 @@ let fruits = [
   },
 ];
 
+// Get all fruits
+router.get("/", (req, res) => {
+  res.json(fruits);
+});
 
-
-
-
-
-
-
+// Get a particular fruit by its index in the array
+router.get("/:id", (req, res) => {
+  const id = req.params.id;
+  if (id < 1 || id > fruits.length) {
+    res.status(404).send("Fruit not found");
+  } else {
+    res.json(fruits[id - 1]);
+  }
+});
 
 module.exports = router;
